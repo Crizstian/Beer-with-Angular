c759ee38756aa2b97929555fa878df72b895a569
Cristian <cristiano.rosetti@gmail.com>
2016-03-09 21:08:26 -0600

    Adding Formbuilder to Contact Component, Adding new images to the dintel

--- client/app/components/contact/contact.compnt.ts
+++ client/app/components/contact/contact.compnt.ts
@@ -1,7 +1,50 @@
 import {Component} from 'angular2/core';
+import {FormBuilder,FORM_DIRECTIVES,
+        ControlGroup,Control,
+        Validators}           from 'angular2/common';
+import {RouteParams, Router,
+        RouterLink}           from 'angular2/router';
+import {CustomValidators}     from '../../Validators/custom-validators';
 
 @Component({
   selector   : 'contact',
+  directives : [FORM_DIRECTIVES,RouterLink],
   templateUrl: 'app/components/contact/templates/contacto.html'
 })
-export class ContactCompnt{}
+export class ContactCompnt{
+
+  group: ControlGroup;
+  formulario:any = {};
+
+  constructor(
+    private _builder: FormBuilder,
+    private _router: Router
+  ) {
+    this.formulario = {
+      nombre: '',
+      email: '',
+      asunto: '',
+      mensaje: ''
+    };
+
+    this.group = this._builder.group({
+      nombre: ['',
+        Validators.compose([Validators.required, CustomValidators.textFormat])
+      ],
+      email: ['',
+        Validators.compose([Validators.required, CustomValidators.emailFormat])
+      ],
+      asunto: ['',
+        Validators.compose([Validators.required, CustomValidators.textFormat])
+      ],
+      mensaje: ['',
+        Validators.compose([Validators.required, CustomValidators.textAreaFormat])
+      ]
+    });
+  }
+
+  submitForm(){
+    console.log(this.formulario);
+  }
+
+}
